5633f12256fee03dce225e19a82547d2fce24902
fathemancre <ymatcheswala@gmail.com>
2016-01-03 16:59:35 -0500

    Initial commit

---
+++ app/src/main/java/com/yankeemikeaviation/helloagain/MainActivity.java
@@ -0,0 +1,76 @@
+package com.yankeemikeaviation.helloagain;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Sets the file activity_main.xml as the user interface
+        setContentView(R.layout.activity_main);
+
+        // To be able to edit the TextView with our code we have to create it and
+        // bind it to a TextView object. I need to use final because it will be
+        // used in the inner class below
+
+        final TextView firstTextView = (TextView) findViewById(R.id.textView);
+
+        //set up the button just like the TextView
+        Button firstButton = (Button) findViewById(R.id.firstButton);
+
+        // This is how you make the Button change the text in the TextView when it is clicked
+        //
+
+        firstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firstTextView.setText("You Clicked the button");
+            }
+        });
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
